2ba6de377742615b63d3346e13abb970e0a96968
nadafirnanda1 <81845916+nadafirnanda1@users.noreply.github.com>
2021-04-22 17:01:04 +0700

    Add files via upload

---
+++ LuasPersegi.java
@@ -0,0 +1,16 @@
+package Latihan1;
+
+import java.util.Scanner;
+
+public class LuasPersegi {
+    public static void main(String[] args) {
+    int sisi, luas;
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Masukkan panjang sisi persegi: ");
+    sisi = scan.nextInt();
+    luas = sisi * sisi;
+    System.out.println("Luas Persegi adalah " + luas);
+  }
+    
+}
+
